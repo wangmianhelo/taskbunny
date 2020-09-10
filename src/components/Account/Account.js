2c035e5ebb14a00fc6d1d3bb31f79ac244127b07
Silvia <hxiaoyin@hotmail.com>
2020-09-10 11:44:05 +1000

    onchange set account state

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Account.module.css'
 
 function Account() {
+    const [accountState, setAccount] = useState({});
+
+
     return (
-        <div className={styles.account__container}>
+        <form className={styles.account__container}>
             <h2 className={styles.account__title}>My details</h2>
             <div className={styles.avatar__row}>
                 <div className={styles.avatar}></div>
@@ -11,20 +14,20 @@ function Account() {
                 <div className={styles.myfile__text}>size no more than 2MB, png/jpeg...</div>
             </div>
             <div className={styles.first__row}>
-                <div>First name:<input type="text"/></div>
-                <div>Last name:<input type="text"/></div>
-                <div>Mobile number:<input type="text"/></div>
+                <div>First name:{accountState.firstname} <input type="text" onChange={ (e)=> setAccount({ ...accountState, firstname: e.target.value }) } value={accountState.firstname} /></div>
+                <div>Last name:<input type="text" onChange={ (e) => setAccount ({ ...accountState, lastname: e.target.value }) } value={accountState.lastname} /></div>
+                <div>Mobile number:<input type="text" onChange={ (e) => setAccount({ ...accountState, mobile: e.target.value }) } value={accountState.mobile} /></div>
             </div>
             <div className={styles.second__row}>
-                <div>Email:<input type="text"/></div>
-                <div>Location:<input type="text"/></div>
+                <div>Email:<input type="text"  onChange={ (e) => setAccount({ ...accountState, email: e.target.value }) } value={accountState.email} /></div>
+                <div>Location:<input type="text" onChange={ (e) => setAccount({ ...accountState, location: e.target.value }) } value={accountState.location} /></div>
             </div>
             <div className={styles.third__row}>
-                <div>ABN:<input type="text"/></div>
-                <div>Company Name:<input type="text"/></div>
+                <div>ABN:<input type="text" onChange={ (e) => setAccount({ ...accountState, abn:e.target.value }) } value={accountState.abn} /></div>
+                <div>Company Name:<input type="text" onChange={ (e) => setAccount({ ...accountState, company: e.target.value }) } value={accountState.company} /></div>
             </div>
             <button className={styles.submit}>Save</button>
-        </div>
+        </form>
     );
 }
 
