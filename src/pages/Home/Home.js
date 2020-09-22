6f8e496b136d7b75a6b1db1c3d426880534f515c
lxf <lixinfang@lixinfangdeMBP.lan>
2020-09-22 22:26:23 +0800

    提交代码

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PopularProjects from "./components/PopularProjects";
 import Others from "./components/Others";
+import Info from "./components/Info";
 
 const Home = () => {
   return (
     <div>
       <PopularProjects />
       <Others />
+      <Info />
     </div>
   );
 };
