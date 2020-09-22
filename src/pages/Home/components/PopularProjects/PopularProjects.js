c36bcbe62146c8299962b73fd5b1f24c3a9bb237
Silvia <hxiaoyin@hotmail.com>
2020-09-22 17:54:24 +1000

    popular project desktop design

--- src/pages/Home/components/PopularProjects/PopularProjects.js
+++ src/pages/Home/components/PopularProjects/PopularProjects.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Project from "./components/Project";
 import styled from "styled-components";
+import pic1 from "./components/Project/pic/pic1.png";
+import pic2 from "./components/Project/pic/pic2.png";
+import pic3 from "./components/Project/pic/pic3.png";
+import pic4 from "./components/Project/pic/pic4.png";
+import pic5 from "./components/Project/pic/pic5.png";
+import pic6 from "./components/Project/pic/pic6.png";
+import pic7 from "./components/Project/pic/pic7.png";
+import pic8 from "./components/Project/pic/pic8.png";
 
 const ProjectContainer = styled.div`
   width: 88.533%;
   margin: auto;
   @media(min-width: 768px) {
-    max-width: 1200px;
+    width: 91.81%;
+    max-width: 1322px;
   }
 
   & > h2 {
@@ -30,18 +39,66 @@ const ProjectContainer = styled.div`
   }
 `
 
+const projects = [
+  {
+    title: "House Cleaning",
+    price: "Starting at $22/hr",
+    link: "/",
+    img: pic1,
+  },
+  {
+    title: "Pipe Cleaning",
+    price: "Starting at $29/hr",
+    link: "/",
+    img: pic2,
+  },
+  {
+    title: "Oven Clean",
+    price: "Starting at $22/hr",
+    link: "/",
+    img: pic3,
+  },
+  {
+    title: "Ground Clean",
+    price: "Starting at $29/hr",
+    link: "/",
+    img: pic4,
+  },
+  {
+    title: "Sofa/furniture Clean",
+    price: "Starting at $20/hr",
+    link: "/",
+    img: pic5,
+  },
+  {
+    title: "Floor Clean",
+    price: "Starting at $22/hr",
+    link: "/",
+    img: pic6,
+  },
+  {
+    title: "Roof Clean",
+    price: "Starting at $22/hr",
+    link: "/",
+    img: pic7,
+  },
+  {
+    title: "Drain Pipe Clean",
+    price: "Starting at $29/hr",
+    link: "/",
+    img: pic8,
+  },
+]
+
+
+
 const PopularProjects = () => {
   return (
     <ProjectContainer>
       <h2>Popular projects in your area</h2>
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <Project />
+      {projects.map(project => (
+        <Project {...project} />
+      ))}
     </ProjectContainer>
   );
 };
--- src/pages/Home/components/PopularProjects/components/Project/Project.js
+++ src/pages/Home/components/PopularProjects/components/Project/Project.js
@@ -10,7 +10,21 @@ const ProjectCard = styled.div`
   width: calc(50% - 1rem);
   float: left;
   @media(min-width: 768px) {
-    width: calc(25% - 1rem);
+    margin: 0 0.9375rem 3rem;
+    width: calc(25% - 1.875rem);
+  }
+`
+
+const ProjectLink = styled.div`
+  display: block;
+  border: 2px solid #E5E5E5;
+  border-radius: 15px;
+
+  & a {
+    text-decoration: none;
+  }
+  & a:visited {
+    color: inherit;
   }
 `
 
@@ -26,22 +40,6 @@ const ProjectImg = styled.div`
   }
 `
 
-const ProjectLink = styled.div`
-  display: block;
-
-  border: 2px solid #E5E5E5;
-  border-radius: 15px;
-  @media(min-width: 768px) {
-    margin: 0 1rem 3rem;
-  }
-
-  & a {
-    text-decoration: none;
-  }
-  & a:visited {
-    color: inherit;
-  }
-`
 const ProjectText = styled.div`
   font-family: Roboto, sans-serif;
   font-size: 0.75rem;
@@ -51,48 +49,44 @@ const ProjectText = styled.div`
   @media(min-width: 768px) {
     font-size: 1.125rem;
     line-height: 1.3125rem;
+    margin: 1.5rem 0 1.5rem 1.875rem;
   }
 
   h3 {
     font-size: 0.75rem;
     font-weight: 700;
     line-height: 0.875rem;
-    margin-bottom: 0.875rem;
+    margin-bottom: 0.5rem;
+    @media(min-width: 768px) {
+      font-size: 1.125rem;
+      line-height: 1.3125rem;
+      margin-bottom: 1rem;
+    }
   }
 
   & span {
-    color: #5EDEFC;
-    &:first-child {
-      display: inline-block;
-      background: #000000;
-      border-radius: 50%;
-      font-size: 0.75rem;
-      width: 0.875rem;
-      height: 0.875rem;
-      line-height: 0.875rem;
-      margin-right: 0.5rem;
-      text-align: center;
-    }
+      color: #5EDEFC;
   }
-    img {
-    background: #5EDEFC;
-    border-radius: 50%;
+
+  img {
     width: 0.875rem;
     height: 0.875rem;
-    padding-bottom: 1px;
-    line-height: 0.875rem;
     margin-right: 0.5rem;
-    vertical-align: bottom;
+    vertical-align: middle;
+    @media(min-width: 768px) {
+      width: 1.6875rem;
+      height: 1.6875rem;
+      margin-right: 1rem;
+    }
   }
 `
 
-const Project = () => {
+const Project = (props) => {
   return(
     <ProjectCard>
       <ProjectLink>
-        <a href="">
-          <ProjectImg src={projectImg}></ProjectImg>
-
+        <a href="/">
+          <ProjectImg src={props.img}></ProjectImg>
           <ProjectText>
             <h3><img src={taskImg} alt="task"/>House Cleaning</h3>
             <span><img src={starImg} alt="star"/>Starting at $22/hr</span>
