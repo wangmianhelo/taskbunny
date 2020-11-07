e13d49df557b97faf2b56905aa7a9691975e5d07
wangmianhelo <18686782752@163.com>
2020-11-07 15:45:33 +1100

    build

---
+++ build/precache-manifest.3a28e6e050632df2c89c5a845223a1bf.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b51608c6a52c130b03b62a6632c8aafb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea8c7a58de756ccb37f",
+    "url": "/static/css/2.47e93a28.chunk.css"
+  },
+  {
+    "revision": "b8172da44d2c20aeb54b",
+    "url": "/static/css/main.2341c925.chunk.css"
+  },
+  {
+    "revision": "1ea8c7a58de756ccb37f",
+    "url": "/static/js/2.c224b9bc.chunk.js"
+  },
+  {
+    "revision": "ba83ac4511b8c9623607b3a86d299511",
+    "url": "/static/js/2.c224b9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8172da44d2c20aeb54b",
+    "url": "/static/js/main.20f4b2c6.chunk.js"
+  },
+  {
+    "revision": "6f11281adaa49ffcb556",
+    "url": "/static/js/runtime-main.0be6e9e6.js"
+  },
+  {
+    "revision": "527e2e1ac74c5f6fe2ef675f7fc0124f",
+    "url": "/static/media/avator.527e2e1a.png"
+  },
+  {
+    "revision": "c8743d6be37710a63f7d63dfd83459e5",
+    "url": "/static/media/date.c8743d6b.svg"
+  },
+  {
+    "revision": "8fe449b5cc790806a922bcb7d0bfa328",
+    "url": "/static/media/first_img.8fe449b5.png"
+  },
+  {
+    "revision": "63088680035c9edd65d79cbca7d5eba4",
+    "url": "/static/media/house-svgrepo-com.63088680.svg"
+  },
+  {
+    "revision": "bde24f1031b4ecfb7923c7e0342fd0cc",
+    "url": "/static/media/id-card-svgrepo-com.bde24f10.svg"
+  },
+  {
+    "revision": "79031390efb0076fc5e7ce6a784ef46d",
+    "url": "/static/media/layer_1.79031390.png"
+  },
+  {
+    "revision": "a42e97757a984c68ff71b91cc1a3f2ef",
+    "url": "/static/media/layer_1.a42e9775.png"
+  },
+  {
+    "revision": "a95a048006c1f5b0bd787d679b775de0",
+    "url": "/static/media/location.a95a0480.svg"
+  },
+  {
+    "revision": "f7954e957bbedd5352d6e633a6a58c95",
+    "url": "/static/media/logo.f7954e95.svg"
+  },
+  {
+    "revision": "37db08a90b8783786b222cf7330e8bff",
+    "url": "/static/media/pic1.37db08a9.png"
+  },
+  {
+    "revision": "ef1e0be91202df91bb1ee8903dd94648",
+    "url": "/static/media/pic2.ef1e0be9.png"
+  },
+  {
+    "revision": "f91138a2f1a4e30139dbad24d1850df1",
+    "url": "/static/media/pic3.f91138a2.png"
+  },
+  {
+    "revision": "4218bc8fbd77d29972092c9ccbf03ea8",
+    "url": "/static/media/pic4.4218bc8f.png"
+  },
+  {
+    "revision": "b984b56235cdce939ec58545fa8b5c2b",
+    "url": "/static/media/pic5.b984b562.png"
+  },
+  {
+    "revision": "f60cd67614f72eaf5bb2c92c603abe3e",
+    "url": "/static/media/pic6.f60cd676.png"
+  },
+  {
+    "revision": "e79b0c6c257f6521189a4cc78810aafe",
+    "url": "/static/media/pic7.e79b0c6c.png"
+  },
+  {
+    "revision": "188ea088d952a9630a65c3e674361428",
+    "url": "/static/media/pic8.188ea088.png"
+  },
+  {
+    "revision": "a392a1712691b93c44cf37447f8280eb",
+    "url": "/static/media/second_img.a392a171.png"
+  },
+  {
+    "revision": "9e8af3de487ce6e7d68ab0f25746de25",
+    "url": "/static/media/star-svgrepo-com.9e8af3de.svg"
+  },
+  {
+    "revision": "abdcaffa7831096f911dfa7da9103a6a",
+    "url": "/static/media/star.abdcaffa.svg"
+  },
+  {
+    "revision": "4f6d61422310178d5026f15861c80977",
+    "url": "/static/media/task.4f6d6142.svg"
+  },
+  {
+    "revision": "4c4d98c4dea856e8de61c9cb6e7ca2e7",
+    "url": "/static/media/tasker-samantha.4c4d98c4.jpg"
+  },
+  {
+    "revision": "15d85519ff4f8f70b3090f1a6f185fcd",
+    "url": "/static/media/taskman.15d85519.jpeg"
+  }
+]);
